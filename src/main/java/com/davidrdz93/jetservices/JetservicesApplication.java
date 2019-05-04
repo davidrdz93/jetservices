05d72e25e2cd3187525365360fdf6bebd358e333
David Rodriguez <davidsantiago.rp@gmail.com>
2019-05-04 17:13:37 +0200

    deploy

--- src/main/java/com/davidrdz93/jetservices/JetservicesApplication.java
+++ src/main/java/com/davidrdz93/jetservices/JetservicesApplication.java
@@ -2,12 +2,19 @@ package com.davidrdz93.jetservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JetservicesApplication {
+public class JetservicesApplication extends SpringBootServletInitializer
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(JetservicesApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JetservicesApplication.class);
+	}
 }
